1c5a9596881377ec1c97cc447adce9156007cf79
George Finch <P2654772@my365.dmu.ac.uk>
2025-03-17 14:07:33 +0000

    Added in some regex so only int can be used.

--- guessingGame.py
+++ guessingGame.py
@@ -1,21 +1,33 @@
-
 import random
+import re #regex
+
+def getUserSelection():
+    userGuess = input("input a number between 0 and 50 ")
+    while(re.fullmatch("^[0-9]*$", userGuess) == None):
+        userGuess = input("invalid input, input a number between 0 and 50 ")
+    else:
+       return int(userGuess)    
+
 
 hiddenNumber = random.randint(0,50);
-print(hiddenNumber)
-userGuess = int(input("input a number between 0 and 50 "))
+userGuessInt = getUserSelection()
 
-while(hiddenNumber != userGuess):
+
+while(hiddenNumber != userGuessInt):
     
     whichSide = ""
-    if hiddenNumber > userGuess:
+    if hiddenNumber > userGuessInt:
          whichSide = "lower"
     else:
          whichSide = "higher"
     print(f"that number is {whichSide} than guess")
 
-    userGuess = int(input("input a number between 0 and 50 "))
+    userGuessInt = getUserSelection()
     
 else:
      print("Congradulations, you have won")
     
+
+
+
+ 
